5ff9be320b7d09b64c882fb230c15c5f841f37e5
Ananya <afmtrendaid@gmail.com>
2023-03-14 00:49:29 -0700

    preivew add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { PersonaldetailsComponent } from './personaldetails/personaldetails.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProgressBarComponent
+    ProgressBarComponent,
+    PersonaldetailsComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html',
   styleUrls: ['./progress-bar.component.css']
 })
 export class ProgressBarComponent {
-progressPercent:Number=30;
+
+progressPercent:number = 0;
+progressColor:String = 'red';
+
+//responsible for changing progress color
+changeProgressColor(){
+
+  //Just to tryout the progress bar
+  //bind it with inserted forms
+  this.progressPercent= this.progressPercent + 20;
+
+
+  //basic if based color change
+if(this.progressPercent >= 100){
+   this.progressPercent=100;
+   this.progressColor = "blue";}
+else if(this.progressPercent < 30) this.progressColor = "red";
+else if(this.progressPercent < 70) this.progressColor = "yellow";
+else if(this.progressPercent < 100) this.progressColor = "green";
+}
 
 }
+
