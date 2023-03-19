b9c1d26ccdffec16b965ce9da650088dea2b0476
Ananya <afmtrendaid@gmail.com>
2023-03-19 11:14:24 -0700

    bug fixes due to forms

--- src/app/personal-summary/personal-summary.component.ts
+++ src/app/personal-summary/personal-summary.component.ts
@@ -11,19 +11,15 @@ export class PersonalSummaryComponent {
 
      //This will be replaces by the backend received data
      public aiSummary:string[] = ["1","2","5","sd", "i am an ai called davoincni"];
-     public personalAddition:FormGroup;
      public aiChoicesExpand:boolean = false;
      public userChoice!: string;
 
-constructor( private resumeService: ResumeService){
-     this.personalAddition = this.resumeService.getReferencesForm();}
+
 
      displayAiChoices (){
       this.aiChoicesExpand = !this.aiChoicesExpand;
      }
-     bindChoice(choice:string){
-      this.personalAddition.value.aisummary = choice;
-     }
+
   
 
 }
