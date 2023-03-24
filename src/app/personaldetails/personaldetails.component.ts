77d8449bb73033281828497c08daddc2d5be2ab2
ffekirnew <ffekirnew0808@gmail.com>
2023-03-24 08:33:17 +0300

    added a new template

--- src/app/personaldetails/personaldetails.component.ts
+++ src/app/personaldetails/personaldetails.component.ts
@@ -1,27 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { SessionStorageService } from 'ngx-webstorage';
 import { ResumeService } from '../services/resume.service';
 
 @Component({
   selector: 'app-personaldetails',
   templateUrl: './personaldetails.component.html',
-  styleUrls: ['./personaldetails.component.css']
+  styleUrls: ['./personaldetails.component.css'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
-export class PersonaldetailsComponent implements OnInit {
+export class PersonaldetailsComponent implements OnInit, OnChanges {
 
   public personalDetailsForm: any;
   public expand: boolean = false;
 
   constructor(
-    private resumeService: ResumeService
+    private resumeService: ResumeService,
+    private sessionStorageService: SessionStorageService
   ) { }
 
   expandPersonal() {
     this.expand = !this.expand;
   }
 
-  ngOnInit(): void {
-    this.personalDetailsForm = this.resumeService.getPersonalDetailsForm();
+  ngOnInit() {
+    try {
+      this.personalDetailsForm = this.sessionStorageService.getItem("personalDetails");
+    } catch {
+      this.personalDetailsForm = this.resumeService.getPersonalDetailsForm();
+    }
+    console.log(this.personalDetailsForm);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.sessionStorageService.store("personalDetails", this.personalDetailsForm);
+    console.log(this.personalDetailsForm);
   }
 
 
