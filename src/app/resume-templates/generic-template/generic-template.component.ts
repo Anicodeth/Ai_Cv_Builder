062e4b7415b5fce4464cb3ffe59ba1c7e7181605
ffekirnew <ffekirnew0808@gmail.com>
2023-04-07 00:41:35 +0300

    added the new pdf converter

--- src/app/resume-templates/generic-template/generic-template.component.ts
+++ src/app/resume-templates/generic-template/generic-template.component.ts
@@ -3,6 +3,7 @@ import { FormGroup } from '@angular/forms';
 import { ImageService } from 'src/app/services/image.service';
 import { PdfService } from 'src/app/services/pdf.service';
 import { ResumeService } from 'src/app/services/resume.service';
+import html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-generic-template',
@@ -55,8 +56,55 @@ export class GenericTemplateComponent implements OnInit {
   }
 
   toPdf() {
-    const dashboard:any = document.getElementById("preview-window");
+    const options = {
+      margin: [0, 0],
+      filename: 'my-document.pdf',
+      image: { type: 'jpeg', quality: 1.0 },
+      html2canvas: { scale: 4, dpi: 300, letterRendering: true },
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+      pagebreak: { mode: 'avoid-all', after: '.page-break' },
+    };
 
-    this.pdfService.toPdf(dashboard);
+    const htmlContent = document.getElementById('preview-window');
+
+    // Get the height of the HTML content
+    const htmlHeight: any = htmlContent?.offsetHeight;
+
+    // Calculate the height of an A4 page in pixels
+    const a4Width = 8.27 * 72; // A4 width in pixels
+    const a4Height = 11.69 * 72; // A4 height in pixels
+
+    // Calculate the number of A4 pages needed to display the content
+    const numPages = Math.ceil(htmlHeight / a4Height);
+
+    // Log the number of pages needed to the console
+    console.log(`HTML content requires ${numPages} A4 pages to display`);
+
+    // Convert the HTML content to PDF using html2pdf
+    html2pdf().from(htmlContent).set(options).outputPdf().then((pdf: any) => {
+      // Do something with the PDF
+    });
+
+    html2pdf().set(options).from(htmlContent).save();
+
+    // html2pdf()
+    //   .from(this.html)
+    //   .set(options)
+    //   .outputPdf()
+    //   .then((pdf: any) => {
+    //     // Do something with the PDF file, e.g. save it to disk or display it in the browser
+    //     const blob = new Blob([pdf], { type: 'application/pdf' });
+    //     const url = URL.createObjectURL(blob);
+    //     window.open(url);
+    //   })
+    //   .catch((error: any) => {
+    //     console.error(error);
+    //   });
   }
+
+  // toPdf() {
+  //   const dashboard:any = document.getElementById("preview-window");
+
+  //   this.pdfService.toPdf(dashboard);
+  // }
 }
